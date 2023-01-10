14c91a9b7f19f97de219c97fb10177b8b2569936
Asen Asenov <53828105+nesabg@users.noreply.github.com>
2023-01-10 18:10:03 +0200

    Feat-Adding_form_and_some_fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,73 @@
-import "./App.css";
+
+import React, { useState } from "react"
 import InputField from "./models/InputField";
 import Select from "./models/Select";
 import Textarea from "./models/Textarea";
 import SubmitBtn from "./models/SubmitBtn";
 
+const listProgramingLangs = [
+  { id: 1, name: "Javascript" },
+  { id: 2, name: "Kotlin" },
+  { id: 3, name: "HTML and CSS" },
+  { id: 4, name: "PHP" },
+];
+
 function App() {
-  const listProgramingLangs = [
-    { id: 1, name: "Javascript" },
-    { id: 2, name: "Kotlin" },
-    { id: 3, name: "HTML and CSS" },
-    { id: 4, name: "PHP" }
-  ];
+  const [name, setName] = useState('') 
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [phone, setPhone] = useState('')
+  const [pLang, setPlang] = useState('Javascript')
+  const [message, setMessage] = useState('')
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault()
+    console.log({
+      name,
+      email,
+      password,
+      phone,
+      pLang,
+      message
+    })
+  }
+
   return (
     <div className="App">
       <h1>Contact form React</h1>
-
-      <InputField
-        inputType="text"
-        inputLabel="Username"
-        placeholder="Gatakka"
-      />
-      <InputField
-        inputType="text"
-        inputLabel="Name"
-        placeholder="Ivan Ivanov"
-      />
-      <Select labelText="Programmin languages" data={listProgramingLangs} />
-      <Textarea labelText="Message" />
-      <SubmitBtn submitValue="submit" />
+      <form onSubmit={e => onSubmitHandler(e)}>
+        <InputField
+          inputType="text"
+          inputLabel="Name"
+          placeholder="Ivan Ivanov"
+          value={name}
+          onChangeFunc={setName}
+        />
+        <InputField
+          inputType="email"
+          inputLabel="Email"
+          placeholder="test@nex-gen.eu"
+          value={email}
+          onChangeFunc={setEmail}
+        />
+        <InputField
+          inputType="password"
+          inputLabel="Password"
+          placeholder="**********"
+          value={password}
+          onChangeFunc={setPassword}
+        />
+        <InputField
+          inputType="number"
+          inputLabel="Phone"
+          placeholder="+359 888 888 888"
+          value={phone}
+          onChangeFunc={setPhone}
+        />
+        <Select labelText="Programmin languages" defaultLang={pLang} data={listProgramingLangs} onChangeFunc={setPlang} />
+        <Textarea labelText="Message" value={message} onChangeFunc={setMessage}/>
+        <SubmitBtn submitValue="submit" />
+      </form>
     </div>
   );
 }
--- src/models/InputField.jsx
+++ src/models/InputField.jsx
@@ -1,11 +1,26 @@
 import React from "react";
 
-function InputField({ inputType, inputLabel, placeholder }) {
-  
+function InputField({
+  inputType,
+  inputLabel,
+  placeholder,
+  value,
+  onChangeFunc,
+}) {
   return (
     <div>
       <label htmlFor={inputLabel}>{inputLabel}: </label>
-      <input id={inputLabel} type={inputType} placeholder={placeholder} name={inputLabel}/>
+      <input
+        id={inputLabel}
+        type={inputType}
+        placeholder={placeholder}
+        name={inputLabel}
+        value={value}
+        autoComplete="on"
+        onChange={(e) => {
+          onChangeFunc(e.target.value);
+        }}
+      />
     </div>
   );
 }
