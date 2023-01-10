816381b77dcd57d1c8fbf139c3711e900d7cbb34
Asen Asenov <53828105+nesabg@users.noreply.github.com>
2023-01-10 17:02:02 +0200

    Feat-Adding_submit_button

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import "./App.css";
 import InputField from "./models/InputField";
 import Select from "./models/Select";
 import Textarea from "./models/Textarea";
+import SubmitBtn from "./models/SubmitBtn";
 
 function App() {
   const listProgramingLangs = [
     { id: 1, name: "Javascript" },
     { id: 2, name: "Kotlin" },
     { id: 3, name: "HTML and CSS" },
-    { id: 4, name: "PHP" },
+    { id: 4, name: "PHP" }
   ];
   return (
     <div className="App">
@@ -24,8 +25,9 @@ function App() {
         inputLabel="Name"
         placeholder="Ivan Ivanov"
       />
-      <Select labelText="Programmin languages" data={listProgramingLangs}/>
+      <Select labelText="Programmin languages" data={listProgramingLangs} />
       <Textarea labelText="Message" />
+      <SubmitBtn submitValue="submit" />
     </div>
   );
 }
